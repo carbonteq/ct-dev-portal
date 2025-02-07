1730646ac24b7461c81b32d650bb947af2340758
carbon-teq <37275129+carbon-teq@users.noreply.github.com>
2025-02-07 18:59:30 +0500

    Styling Aside component for whole site

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
 				light: "./src/assets/logo-light.svg",
 				dark: "./src/assets/logo-dark.svg",
 			},
-			favicon: "public/favicon.svg",
+			favicon: "/favicon.svg",
 			social: {
 				github: "https://github.com/withastro/starlight",
 			},
